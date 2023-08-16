848968bced4a98bc6ee5d5ee97aa0991821f3ee3
yuchenbao <yuchenbao@chenbaoMacBook-Pro.local>
2023-08-16 11:26:42 +0200

    open project

---
+++ src/assets/images/activitiesLogo/main.js
@@ -0,0 +1,7 @@
+import ajcf from './ajcf.jpeg';
+import dessin from './dessin.jpeg';
+import funny from './funny.jpeg';
+import hanfu from './hanfu.jpeg';
+import rando from './rando.jpeg';
+
+export {ajcf, dessin, funny, hanfu, rando};
